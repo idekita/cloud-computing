e71d980f24d78b7a793061c627863679abd2f5f7
Dhillen Brahmantya Pradifta <102419678+dhillen-bp@users.noreply.github.com>
2023-06-11 01:18:28 +0700

    Update projectHandler.js

--- src/handlers/projectHandler.js
+++ src/handlers/projectHandler.js
@@ -10,11 +10,12 @@ const { Storage } = require("@google-cloud/storage");
 const { Sequelize } = require("sequelize");
 
 // const storage = new Storage();
+console.log("cek " + process.env._SERVICE_ACCOUNT_KEY);
 const storage = new Storage({
   projectId: "ide-kita",
   keyFilename: JSON.parse(process.env._SERVICE_ACCOUNT_KEY),
 });
-console.log("cek " + process.env._SERVICE_ACCOUNT_KEY);
+
 const bucketName = "project-imgs";
 const bucket = storage.bucket(bucketName);
 
